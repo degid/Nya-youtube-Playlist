f1f3df607ba250938df8a8af9131683f36709973
degid <sdegid@gmail.com>
2017-06-20 21:11:58 +0300

    up

--- Nya youtube Playlist.py
+++ Nya youtube Playlist.py
@@ -19,25 +19,13 @@ appSetting['auth_uri'] = "https://accounts.google.com/o/oauth2/auth"
 appSetting['token_uri'] = "https://accounts.google.com/o/oauth2/token"
 appSetting['client_secret'] = 'wTQnAhwHl7i7WESBq6Vtx4eN'
 
-dirName = 'Nya youtube Playlist'
-if sys.platform == 'win32':
-    user_path = os.getenv('APPDATA')
-elif sys.platform == 'linux':
-    user_path = os.path.expanduser("~")
-    dirName = '.' + dirName
-
-program_path = user_path + '/' + dirName + '/'
-if not os.path.exists(program_path):
-    os.mkdir(program_path)
-
 class MainWindow(QWidget):
     def __init__(self, parent=None):
         QWidget.__init__(self, parent)
         #self.resize(700, 400)
 
-        self.requests = requests()
-
         self.OAuth20Data = {}
+        self.serviceGoogle = serviceGoogle()
 
         self.icon = QIcon('terrible.png')
         self.setWindowIcon(self.icon)
@@ -50,57 +38,41 @@ class MainWindow(QWidget):
         quitAction.triggered.connect(QCoreApplication.instance().quit)
         self.tray.setContextMenu(self.menu)
 
-        if not self.loadToken():
+        if not self.serviceGoogle.loadDataAccess():
             self.show()
-            OAuth20url = "?response_type=code&access_type=offline&" \
-                         "client_id=" + appSetting['client_id'] + "&" \
-                         "redirect_uri=" + appSetting['redirect_uri'] + "&" \
-                         "scope=" + appSetting['scope']
 
             self.browser = QWebEngineView()
             self.browser.titleChanged['QString'].connect(self.titleLoad)
-            self.browser.load(QUrl(appSetting['auth_uri'] + OAuth20url))
+            self.browser.load(QUrl(appSetting['auth_uri'] + self.serviceGoogle.OAuth20url))
 
             self.grid = QGridLayout()
             self.grid.addWidget(self.browser, 0, 0)
             self.setLayout(self.grid)
-
         else:
             self.show_tray()
             #print('nya22')
-            print(self.OAuth20Data['access_token'])
-            self.subscriptionsGo()
-
-    def subscriptionsGo(self):
-        if not self.subscriptions():
-            print('U2', 'если была ошибка, то пробуем ещё раз')
-            self.subscriptions()
+            print(self.serviceGoogle.OAuth20Data)
+            rsp = self.subscriptions()
+            print(rsp)
 
     def subscriptions(self):
-        headers = {'Authorization': 'Bearer ' + self.OAuth20Data['access_token']}
-        params = str(urlparse.urlencode({'part': 'snippet,contentDetails', 'mine': 'true'}).encode('ascii'))[2:-1]
+        response = False
+        if self.serviceGoogle.checkToken():
+            headers = {'Authorization': 'Bearer ' + self.serviceGoogle.OAuth20Data['access_token']}
+            params = str(urlparse.urlencode({'part': 'snippet,contentDetails', 'mine': 'true'}).encode('ascii'))[2:-1]
 
-        response = self.requests.request('GET', 'https://www.googleapis.com/youtube/v3/subscriptions?' + params, headers)
-        print(response)
-        err = self.responseError(response)
-        print('err', err[0], err[1])
-        if not err[0]:
-            print('U1', 'всё ок')
-            return True
-        else:
-            if err[1]:
-                print('U3', 'была ошибка, пробуем ещё')
-                return False
-            else:
-                print('U3', 'была ошибка, хватит')
-                return True
-        return True
+            response = self.serviceGoogle.request('GET', 'https://www.googleapis.com/youtube/v3/subscriptions?' + params, headers)
+
+            err = self.responseError(response)
+            print(err)
+        return response
+
 
     def responseError(self, response):
         if response['code'] == 401:
             if self.getToken():
                 self.saveToken()
-                print(self.OAuth20Data['access_token'])
+                print(self.serviceGoogle.OAuth20Data['access_token'])
                 return True, True
         if response['code'] == 400:
             #print(json.loads(response['ResponseText']))
@@ -117,18 +89,6 @@ class MainWindow(QWidget):
     def __del__(self):
         self.tray.hide()
 
-    def saveToken(self):
-        with open(program_path + 'token.pkl', 'wb') as token_pickle:
-            pickle.dump(self.OAuth20Data, token_pickle)
-
-    def loadToken(self):
-        if os.path.exists(program_path + 'token.pkl'):
-            with open(program_path + 'token.pkl', 'rb') as token_pickle:
-                self.OAuth20Data = pickle.load(token_pickle)
-            return True
-        else:
-            return False
-
     def titleLoad(self, title):
         if self.badtitle:
             print('Что за хрень?????')
@@ -136,33 +96,75 @@ class MainWindow(QWidget):
         if title.find('Success code=') != -1:
             print('Nya???????')
             self.badtitle = True
-            print(title)
-            self.OAuth20Data['code'] = title[13:]
-            self.saveToken()
+            self.serviceGoogle.OAuth20Data['code'] = title[13:]
+            self.serviceGoogle.saveDataAccess()
             self.browser.close()
             self.hide()
             self.show_tray()
 
-            if self.getToken(False):
-                self.saveToken()
+            self.serviceGoogle.getToken(False)
 
-            self.subscriptionsGo()
+            print(self.subscriptions())
+
+
+class serviceGoogle:
+    def __init__(self):
+        self.OAuth20Data = {}
+
+        self.OAuth20url = "?response_type=code&access_type=offline&" \
+                     "client_id=" + appSetting['client_id'] + "&" \
+                     "redirect_uri=" + appSetting['redirect_uri'] + "&" \
+                     "scope=" + appSetting['scope']
+
+        dirName = 'Nya youtube Playlist'
+        if sys.platform == 'win32':
+            user_path = os.getenv('APPDATA')
+        elif sys.platform == 'linux':
+            user_path = os.path.expanduser("~")
+            dirName = '.' + dirName
+
+        self.user_setting_path = user_path + '/' + dirName + '/'
+        if not os.path.exists(self.user_setting_path):
+            os.mkdir(self.user_setting_path)
+
+    def checkToken(self):
+        tokenLiveTime = (self.OAuth20Data['expires_in'] - self.OAuth20Data['expires_in'] * .1)
+        timeNow = time.time()
+        if tokenLiveTime + self.OAuth20Data['time'] < timeNow:
+            return self.getToken()
+        else:
+            return True
+
+    def loadDataAccess(self):
+        if os.path.exists(self.user_setting_path + 'token.pkl'):
+            with open(self.user_setting_path + 'token.pkl', 'rb') as token_pickle:
+                self.OAuth20Data = pickle.load(token_pickle)
+            return True
+        else:
+            return False
+
+    def saveDataAccess(self):
+        with open(self.user_setting_path + 'token.pkl', 'wb') as token_pickle:
+            pickle.dump(self.OAuth20Data, token_pickle)
 
     def getToken(self, stage=True):
         if stage:
             params = {'grant_type': 'refresh_token', 'refresh_token': self.OAuth20Data['refresh_token']}
             print('T1')
         else:
-            params = {'grant_type': 'authorization_code', 'code': self.OAuth20Data['code'], 'redirect_uri':appSetting['redirect_uri']}
+            params = {'grant_type': 'authorization_code', 'code': self.OAuth20Data['code'],
+                        'redirect_uri': appSetting['redirect_uri']}
             print('T2')
 
         params['client_id'] = appSetting['client_id']
         params['client_secret'] = appSetting['client_secret']
         params = urlparse.urlencode(params).encode('ascii')
 
-        response = self.requests.request('POST', appSetting['token_uri'], {'Content-Type': 'application/x-www-form-urlencoded'}, params)
+        response = self.request('POST', appSetting['token_uri'],
+                                             {'Content-Type': 'application/x-www-form-urlencoded'}, params)
         ResponseDict = json.loads(response['ResponseText'])
 
+        result = False
         if response['code'] == 200:
             self.OAuth20Data['token_type'] = ResponseDict['token_type']
             self.OAuth20Data['expires_in'] = ResponseDict['expires_in']
@@ -170,13 +172,10 @@ class MainWindow(QWidget):
             self.OAuth20Data['access_token'] = ResponseDict['access_token']
             if not stage:
                 self.OAuth20Data['refresh_token'] = ResponseDict['refresh_token']
-            return True
-        else:
-            return False
+            self.saveDataAccess()
+            result = True
+        return result
 
-class requests:
-    def __init__(self):
-        pass
 
     def parseResponse(self, data):
         response = {}
@@ -234,8 +233,8 @@ class requests:
         return self.parseResponse(data)
 
 if __name__ == '__main__':
-        app = None
-        if not QApplication.instance():
-            app = QApplication([])
-        dlg = MainWindow()
-        if app: app.exec_()
+    app = None
+    if not QApplication.instance():
+        app = QApplication([])
+    dlg = MainWindow()
+    if app: app.exec_()
